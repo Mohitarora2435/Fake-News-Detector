9958d266e55913f9ac01cb97ac335f6f427e9c04
Mohit <mohitarora2435@gmail.com>
2025-03-22 09:07:26 +0530

    Fix

--- setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-pip install --upgrade pip
-pip install --no-cache-dir --force-reinstall scikit-learn
-pip install --no-cache-dir -r requirements.txt || echo "No requirements.txt found, skipping..."
