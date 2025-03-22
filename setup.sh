ebbfa728db59b6a190c6b40cb29c2ad2c1a32d72
Mohit Arora <92647348+Mohitarora2435@users.noreply.github.com>
2025-03-22 05:45:43 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pip install --upgrade pip
-pip install --no-cache-dir scikit-learn
-pip install --no-cache-dir -r requirements.txt
+pip install --no-cache-dir --force-reinstall scikit-learn
+pip install --no-cache-dir -r requirements.txt || echo "No requirements.txt found, skipping..."
